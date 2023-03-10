e7b471143403d4f095210393ddaa072e3b177be8
AlphaCube13 <122994645+AlphaCube13@users.noreply.github.com>
2023-03-10 21:05:24 +0600

    lol
    f

---
+++ jjajajajjajaahhheeellooWwwwwwworld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jjajajajjajaahhheeellooWwwwwwworld : MonoBehaviour
+{
+    public GameObject AAAAAAAAAAAAAA;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AAAAAAAAAAAAAA.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("crickets");
+    }
+}
